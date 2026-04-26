4dbfef8b7cf211d02085c596f260b5a9e316dbc9
Charles Sherwood <kulan876321@yahoo.com>
2026-04-26 00:48:51 -0400

    Wrote the code for the Basicshape header file for the class

--- BasicShapesPrj/Basicshape.h
+++ BasicShapesPrj/Basicshape.h
@@ -1,5 +1,18 @@
 #pragma once
-class Basicshape
-{
-};
+#include <string>
+using namespace std;
 
+class BasicShape {
+private:
+    double area;
+    string name;
+
+public:
+    double getArea() const { return area; }
+    void setArea(double a) { area = a; }
+
+    string getName() const { return name; }
+    void setName(string n) { name = n; }
+
+    virtual void calcArea() = 0;
+};
