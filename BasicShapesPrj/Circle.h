090678e7996fc19b47a80d457473ad19d64da8df
Charles Sherwood <kulan876321@yahoo.com>
2026-04-26 00:49:49 -0400

    Wrote the code for the Circle class header file

--- BasicShapesPrj/Circle.h
+++ BasicShapesPrj/Circle.h
@@ -1,5 +1,22 @@
 #pragma once
-class Circle
-{
-};
+#include "BasicShape.h"
 
+class Circle : public BasicShape {
+private:
+    double xCenter;
+    double yCenter;
+    double radius;
+
+public:
+    Circle(double x, double y, double r, string n = "Circle");
+
+    void calcArea() override;
+
+    double getXCenter() const { return xCenter; }
+    double getYCenter() const { return yCenter; }
+    double getRadius() const { return radius; }
+
+    void setXCenter(double x) { xCenter = x; }
+    void setYCenter(double y) { yCenter = y; }
+    void setRadius(double r) { radius = r; calcArea(); }
+};
