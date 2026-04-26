41e6af94708de28e2897fff83ade026a7abe88b7
Charles Sherwood <kulan876321@yahoo.com>
2026-04-26 00:55:33 -0400

    Wrote the code for the square source cpp code

--- BasicShapesPrj/Square.cpp
+++ BasicShapesPrj/Square.cpp
@@ -1,1 +1,13 @@
 #include "Square.h"
+
+Square::Square(double s, string n)
+    : Rectangle(s, s, n) {
+    side = s;
+    setName(n);
+}
+
+void Square::setSide(double s) {
+    side = s;
+    setLength(s);
+    setWidth(s);
+}
