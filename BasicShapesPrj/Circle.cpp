92724fa8f69d1e01d5ff82d798d3a0288aae8cbf
Charles Sherwood <kulan876321@yahoo.com>
2026-04-26 00:52:10 -0400

    Wrote the cpp source code for the Circle class

--- BasicShapesPrj/Circle.cpp
+++ BasicShapesPrj/Circle.cpp
@@ -1,1 +1,22 @@
 #include "Circle.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include<iostream>
+
+#include <numbers>
+double M_PI = numbers::pi;
+
+
+using namespace std;
+
+Circle::Circle(double x, double y, double r, string n) {
+    xCenter = x;
+    yCenter = y;
+    radius = r;
+    setName(n);
+    calcArea();
+}
+
+void Circle::calcArea() {
+    setArea(M_PI * radius * radius);
+}
