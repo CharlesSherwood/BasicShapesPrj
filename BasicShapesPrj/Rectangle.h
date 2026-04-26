0dcc15e035a0d4e588808a94f6c65daf69367654
Charles Sherwood <kulan876321@yahoo.com>
2026-04-26 00:53:07 -0400

    Wrote the class header file for the rectangle class

--- BasicShapesPrj/Rectangle.h
+++ BasicShapesPrj/Rectangle.h
@@ -1,5 +1,19 @@
 #pragma once
-class Rectangle
-{
-};
+#include "BasicShape.h"
 
+class Rectangle : public BasicShape {
+private:
+    double length;
+    double width;
+
+public:
+    Rectangle(double l, double w, string n = "Rectangle");
+
+    void calcArea() override;
+
+    double getLength() const { return length; }
+    double getWidth() const { return width; }
+
+    void setLength(double l) { length = l; calcArea(); }
+    void setWidth(double w) { width = w; calcArea(); }
+};
