83e30f75440b067b222f877afd3b89c807bb2560
Charles Sherwood <kulan876321@yahoo.com>
2026-04-26 00:53:59 -0400

    Finished the code for the Rectangle cpp source code that is used for the class rectangle

--- BasicShapesPrj/Rectangle.cpp
+++ BasicShapesPrj/Rectangle.cpp
@@ -1,1 +1,12 @@
 #include "Rectangle.h"
+
+Rectangle::Rectangle(double l, double w, string n) {
+    length = l;
+    width = w;
+    setName(n);
+    calcArea();
+}
+
+void Rectangle::calcArea() {
+    setArea(length * width);
+}
